872f150fd408d6dba7d17df33da245e9a6173ad8
QuentinRospars <quentin.rospars@valtech.com>
2023-09-17 00:05:35 +0200

    Text animations on scroll and when page loads

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,20 +1,107 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const WelcomeSection = () => {
+    const animationRefs = [
+        { ref: useRef<HTMLHeadingElement | null>(null), direction: 'right' },
+        { ref: useRef<HTMLHeadingElement | null>(null), direction: 'left' },
+    ];
+
+    const appElementRef = useRef<HTMLElement | null>(null);
+
+    useEffect(function initAnimation() {
+        // Disable scroll
+        document.body.style.overflow = 'hidden';
+
+
+        // Initialize Observers
+        const leftObserver = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("slide-from-left");
+                    leftObserver.unobserve(entry.target);
+                }
+            },
+            { root: appElementRef.current, threshold: 0.1 }
+        );
+
+        const rightObserver = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("slide-from-right");
+                    rightObserver.unobserve(entry.target);
+                }
+            },
+            { root: appElementRef.current, threshold: 0.1 }
+        );
+
+
+        // Attach observer to each ref
+        animationRefs.forEach((ref) => {
+            if (ref.ref.current) {
+                if (ref.direction === 'left') {
+                    leftObserver.observe(ref.ref.current);
+                } else {
+                    rightObserver.observe(ref.ref.current);
+                }
+            }
+        });
+
+        // Cache elements
+        appElementRef.current = document.querySelector('.App') as HTMLElement | null;
+        const slideFromTopElement = document.getElementById("title");
+        const subtitleElement = document.getElementById("subtitle");
+        const mainTextElement = document.getElementById("tagline");
+        const animationDuration = 2500;
+
+        // Add initial animations
+        if (slideFromTopElement && subtitleElement && mainTextElement) {
+            slideFromTopElement.classList.add("slide-from-top");
+            subtitleElement.classList.add("fade-in");
+            mainTextElement.classList.add("slide-from-left--delayed");
+        }
+
+        const timeoutId = setTimeout(() => {
+            requestAnimationFrame(() => {
+                if (appElementRef.current) {
+                    console.log('scrolling allowed')
+                    appElementRef.current.style.overflowY = 'auto';
+                }
+            });
+
+        }, animationDuration);
+
+
+        // Clean-up
+        return () => {
+            clearTimeout(timeoutId);
+            animationRefs.forEach((ref) => {
+                if (ref.ref.current) {
+                    if (ref.direction === 'left') {
+                        leftObserver.unobserve(ref.ref.current);
+                    } else {
+                        rightObserver.unobserve(ref.ref.current);
+                    }
+                }
+            });
+        };
+    }, []);
+
+
+
 
     return (
-        <section className="welcome">
+        <section id='welcome' className="welcome">
             <div className="screen welcome__titleContainer">
-                <h1 className="title welcome__title">[SRAPSPROJECT001]</h1>
-                <p className="subtitle welcome__subtitle"><span className='aclonica '>The</span> First Chapter <span className='aclonica '>Begins</span></p>
-                <p className="tagline welcome__tagline italic">A unique <span className='aclonica regular'>fusion</span> of <br /><span className='aclonica regular padding-left-15'>generative design</span> and <span className='regular aclonica '>house music</span>, waiting to be <br /><span className='aclonica regular padding-left-5'>discovered</span>.</p>
+                <h1 id='title' className="title welcome__title hidden">[SRAPSPROJECT001]</h1>
+                <p id='subtitle' className="subtitle welcome__subtitle hidden">The First Chapter Begins</p>
+                <p id='tagline' className="tagline welcome__tagline italic hidden">A unique <span className='aclonica regular'>fusion</span> of <br /><span className='aclonica regular padding-left-15'>generative design</span> and <span className='regular aclonica '>house music</span>, waiting to be <br /><span className='aclonica regular padding-left-5'>discovered</span>.</p>
             </div>
             <div className="screen welcome__content">
-                <div className='welcome__content__journey'>
-                    <h2 className="subtitle">The Journey</h2>
+                <div ref={animationRefs[0].ref} className='welcome__content__journey hidden'>
+                    <h2 className="subtitle ">The Journey</h2>
                     <p className="text ">Dive into a blend of visual art and house beats. SRAPSPROJECT001 isn't just music; it's a unique experience born from mathematics and randomness, crafting a musical journey that's non-replicable.</p>
                 </div>
-                <div className='welcome__content__concept'>
+                <div ref={animationRefs[1].ref} className='welcome__content__concept hidden'>
                     <h2 className="subtitle">The Concept</h2>
                     <p className="text ">A hundred vinyls. A hundred designs. Scattered across Europe, they await a lucky few. Find one and be part of our story.</p>
                 </div>
